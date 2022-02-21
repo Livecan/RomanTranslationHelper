d50903b60368c9c4650aee034a9fc8f7c18668d5
Livecan <roman.meszaros@gmail.com>
2022-02-21 14:24:04 +0200

    ADDED Loading list of the 7 translations of module

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,6 +9,13 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  * Translation Helper default Controller.
  */
 class DefaultController extends ControllerBase {
+
+  /**
+   * Get element that mounts React App front-end.
+   *
+   * @return array
+   *   Drupal Render array of element which mount the React App front-end.
+   */
   public function getApp() {
     return [
       '#markup' => '<div id="roman-translation-helper-react-app" />',
@@ -21,7 +28,42 @@ class DefaultController extends ControllerBase {
     ];
   }
 
+  /**
+   * Download language files for module.
+   *
+   * @param string $moduleName
+   *   Module name.
+   *
+   * @return Symfony\Component\HttpFoundation\JsonResponse
+   *   Json response.
+   */
   public function downloadLanguageFiles($moduleName) {
-    return (new JsonResponse([file_get_contents("http://ftp.drupal.org/files/translations/all/$moduleName/")]));
+    $moduleLocalizationHubHtml = file_get_contents("http://ftp.drupal.org/files/translations/all/$moduleName/");
+
+    $output = [];
+
+    $languages = ['ar', 'es', 'fil', 'fr', 'hi', 'id', 'ru'];
+
+    foreach ($languages as $language) {
+      preg_match_all('/href="([^"]*\.' . $language . '\.po)"/', $moduleLocalizationHubHtml, $matches);
+
+      $filename = $matches[1][count($matches[1]) - 1];
+
+      $poFileStream = fopen("http://ftp.drupal.org/files/translations/all/$moduleName/$filename", "r");
+      $msgId = NULL;
+      while ($line = fgets($poFileStream)) {
+        if (preg_match('/msgid "([^"]*)"/', $line, $matches)) {
+          $msgId = $matches[1];
+        }
+        elseif ($msgId && preg_match('/msgstr "([^"]*)"/', $line, $matches)) {
+          $msgStr = $matches[1];
+          $output[$language][$msgId] = $msgStr;
+        }
+      }
+
+    }
+
+    return (new JsonResponse($output));
   }
+
 }
--- src/jsx/DownloadTranslationsPage.jsx
+++ src/jsx/DownloadTranslationsPage.jsx
@@ -1,20 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useMemo, useState } from "react";
 import axios from "axios";
 import { Stack, TextField, Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { Done } from '@mui/icons-material';
 
 export default () => {
-  useEffect(() => {
+  const [moduleName, setModuleName] = useState(null);
+  const [localizationData, setLocalizationData] = useState([]);
+
+  const load = () => {
     axios.get(
-      "/roman-translation-helper/download/linkchecker?format=_json"
+      `/roman-translation-helper/download/${moduleName}?format=_json`
     )
-      .then(response => console.log(response.data))
+      .then(response => {
+        setLocalizationData(response.data);
+      })
       .catch(error => console.error(error));
-  }, []);
+  };
+
+  const localizationTable = useMemo(() => {
+      let table = {};
+      for (const [language, languageData] of Object.entries(localizationData)) {
+        for (const phraseId in languageData) {
+          if (Object.hasOwnProperty.call(languageData, phraseId)) {
+            if (!table[phraseId]) {
+              table[phraseId] = {};
+            }
+            table[phraseId][language] = true;
+
+          }
+        }
+      }
+      return table;
+    },
+    [localizationData]
+  );
 
   return (
-    <Stack direction="row" spacing="1rem">
-      <TextField placeholder="Enter module name" />
-      <Button>Search</Button>
+    <Stack direction="column" spacing="1rem">
+      <Stack direction="row" spacing="1rem">
+        <TextField placeholder="Enter module name" onChange={e => setModuleName(e.currentTarget.value)} />
+        <Button onClick={load}>Search</Button>
+      </Stack>
+      <DataGrid
+        autoHeight
+        columns={[
+          {
+            field: "phrase",
+            headerName: "Phrase",
+            flex: 1,
+          },
+          ...(Object.keys(localizationData).map(language => (
+            {
+              field: language === "id" ? "_id" : language,
+              headerName: language,
+              width: 20,
+              renderCell: (params) => (
+                params.value &&
+                  <Done />
+              )
+            }
+          )))
+          ]
+        }
+        rows={Object.entries(localizationTable).map(([phrase, translations]) => {
+          let row = {
+            id: phrase,
+            phrase: phrase,
+          }
+          for (const language in translations) {
+            if (Object.hasOwnProperty.call(translations, language)) {
+              row[language === "id" ? "_id" : language] = 1;
+            }
+          }
+          return row;
+        })}
+      />
     </Stack>
   );
 };
